ac7f9adc1ee5c347eea74ba2f1d7680b73966fe9
adellelin <adelle.lin@gmail.com>
2019-01-16 22:24:11 -0800

    added  the ability to read the color of incoming imu jacket colors

--- TinyGang/TinyGang.ino
+++ TinyGang/TinyGang.ino
@@ -1,9 +1,9 @@
 /*
- * Tiny Gang is an open source project to include multiple jacket wearers
- * the base code is developed by @mpinner and @adellelin for two jacket
- * interaction and is to be expanded for multiple players
- */
- 
+   Tiny Gang is an open source project to include multiple jacket wearers
+   the base code is developed by @mpinner and @adellelin for two jacket
+   interaction and is to be expanded for multiple players
+*/
+
 #include "FastLED.h"
 
 #include <elapsedMillis.h>
@@ -15,20 +15,19 @@
 #include "BookendFlip.h"
 
 // How many leds in your strip?
-#define NUM_LEDS 38
+#define NUM_LEDS 42
 
 #define JACKET1_LEFT  1
 #define JACKET1_RIGHT 2
 #define JACKET2_LEFT   11
 #define JACKET2_RIGHT  12
 
-
 #define JACKET1 1 // use this when other jacket is HR, otherwise 0
 #define JACKET2 0 // we used JACKET2 0 for white jacket
 
 #define WHOSE_HARDWARE JACKET1 // little black jacet with one long strip split to two connectors
 
-#define ADC_TEENSY_3_5 // change this depending on which board
+#define ADC_TEENSY_3_1 // change this depending on which board
 
 #ifdef ADC_TEENSY_3_1 // sets up pin conditions for the 3.2 boards
 #define DATA_PIN_RIGHT 23
@@ -42,7 +41,7 @@
 #define CLOCK_PIN_LEFT 3
 #endif
 
-#define MY_PATTERN_ID 1
+#define MY_PATTERN_ID 4
 
 Pattern* patterns[] = {
   new BookendFlip(), // pattern individually triggered
@@ -53,11 +52,14 @@ Pattern* patterns[] = {
   new Twinkler()
 };
 
+int inboundHue = 229; // incoming char sets color
+
 int ledHue[] = { 0, 20, 255, 229, 200, 120};
 // 120 was cyan
 // 229 pink
 // 22 orange
 // 200 lilac
+// 'a' green
 
 elapsedMillis ellapseTimeMs[(sizeof(patterns)) / 4];
 
@@ -66,30 +68,27 @@ float regularDuration = 3000;
 float fastDuration = 2000;
 float durationMs = crazyDuration;
 
-float repeatDurationMs = 11000;
+float repeatDurationMs = 17000;
 float repeatCrazyMs = 3000;
 boolean sentAlready = false; // send message boolean
 
 elapsedMillis sendEllapseMs;
 float sendMs = 2000;
 
-
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
-
-
-CRGB led[(sizeof(patterns)) / 4][NUM_LEDS];
+CRGB led[(sizeof(patterns)) / 4][NUM_LEDS]; //
 
 // Define the array of leds
 CRGB leds_right[NUM_LEDS];
 CRGB leds_left[NUM_LEDS];
 
-char soundCommand[] = {
-  'q', 'a', 'z',  // mushroom 0 is when all off
-  'w', 's', 'x',  // mushroom 1 is when only 4 on
-  'e', 'd', 'c',  // mushroom 2 is when only 3 on
-  'r', 'f', 'v'   // mushroom 3 is when 3 and 4 on
+char patternCommand[] = {
+  'q', 'a', 'z',
+  'w', 's', 'x',
+  'e', 'd', 'c',
+  'r', 'f', 'v'
 };
 
 void setup() {
@@ -97,7 +96,6 @@ void setup() {
   // setup serial DEBUG and XBEE
   Serial.begin(9600);
   Serial1.begin(9600);
-
 
   // setup pins for JACKET1 vs JACKET2
   pinMode(10, INPUT_PULLUP);
@@ -112,25 +110,24 @@ void setup() {
   LEDS.setDither( 0 );
   FastLED.setDither( 0 );
 
-        
   Serial.print("uploaded.. ");
   Serial.println(__FILE__);
-        
+
   Serial.print("reseting jacket.. ");
   Serial.println(__DATE__);
 
-
   Serial.print("jacket switch is");
+  // For adding a switch to have a pattern that is always on vs regular
   if (isCrazy()) {
     Serial.println(" CRAZY");
   }
   if (isRegular()) {
     Serial.println(" REGULAR");
   }
-        
 
   for (int i = 0; i < (sizeof(patterns)) / 4; i++) {
     for (int j = 0; j < NUM_LEDS; j ++) {
+      //led[i][j] = CHSV(100,100,100);
       led[i][j] = CHSV(255 / NUM_LEDS * j, 255 / (sizeof(patterns)) / 4 * i, 255);
     }
   }
@@ -148,29 +145,21 @@ void loop() {
 
     char inChar = (char)Serial1.read();
     Serial.println(inChar);
-    //Serial.println(sizeof(soundCommand));
-    //Serial.println(sizeof(patterns)/4);
-    if (inChar == soundCommand[0]) {
-      ellapseTimeMs[0] = 0;
-      durationMs = fastDuration;
-    } else if (inChar == soundCommand[1]) {
-      ellapseTimeMs[1] = 0;
-      durationMs = regularDuration;
-    } else if (inChar == soundCommand[2]) {
-      ellapseTimeMs[2] = 0;
-      durationMs = crazyDuration;
-    } else if (inChar == soundCommand[3]) {
-      ellapseTimeMs[3] = 0;
-      durationMs = fastDuration;
-    } else if (inChar == soundCommand[4]) {
-      ellapseTimeMs[4] = 0;
-      durationMs = crazyDuration;
-    } else if (inChar == soundCommand[5]) {
-      ellapseTimeMs[5] = 0;
-      durationMs = crazyDuration;
+    //Serial.println(sizeof(patternCommand));
+    Serial.println(sizeof(patterns) / 4);
+    // read the incoming character and check it's index,
+    // start the corresponding pattern ID by setting its timer to 0
+    for  (int i = 0; i < sizeof(patternCommand); i++) {
+      if (inChar == patternCommand[i]) {
+        ellapseTimeMs[i] = 0;
+        //durationMs = regularDuration;
+      } else {
+        // if incoming characters do not correspond to pattern ID
+        // it sets the jacket color
+        inboundHue = inChar;
+      }
     }
   }
-
 
   /*
     // autosend every 2s
@@ -195,9 +184,10 @@ void loop() {
       Serial.print("sending::");
       Serial.print(" my-pattern-id:");
       Serial.print(MY_PATTERN_ID);
+      Serial1.print((char)ledHue[MY_PATTERN_ID]); // sending color to IMU jackets
       Serial.print(" cmd:");
-      Serial.println(soundCommand[MY_PATTERN_ID]);
-      Serial1.print(soundCommand[MY_PATTERN_ID]);
+      Serial.println(patternCommand[MY_PATTERN_ID]);
+      Serial1.print(patternCommand[MY_PATTERN_ID]);
       sentAlready = true;
 
     }
@@ -207,11 +197,18 @@ void loop() {
     for (int j = 0; j < NUM_LEDS; j ++) {
 
       if (ellapseTimeMs[i] > durationMs ) {
+        //led[i][j] = CHSV(100,100,100);
         led[i][j] = 0; // turn all LED to black once time hits
       } else {
         float position = j / (float)NUM_LEDS;
         float remaining = 1.0 - ellapseTimeMs[i] / durationMs;
-        led[0][j] = patterns[i]->paintLed (position, remaining, led[0][j], ledHue[4]);
+        // set color to be the incoming message color
+        int color = inboundHue;
+        // if own pattern, then lookup color array
+        if (i == MY_PATTERN_ID) {
+          color = ledHue[i];
+        }
+        led[0][j] = patterns[i]->paintLed (position, remaining, led[0][j], color);
         //Serial.println(ledHue[i]);
       }
 
@@ -225,7 +222,7 @@ void loop() {
 
 
 void show() {
-  int half_leds = (NUM_LEDS/2);
+  int half_leds = (NUM_LEDS / 2);
 
   switch (WHOSE_HARDWARE) {
     case JACKET1 :
