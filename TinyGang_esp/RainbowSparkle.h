6f3c2ae21c95dbf205a1d2b4d917ccab4bd86f14
adellelin <adelle.lin@gmail.com>
2019-10-04 23:27:55 -0700

    prevents sparkle going to red

--- TinyGang_esp/RainbowSparkle.h
+++ TinyGang_esp/RainbowSparkle.h
@@ -9,8 +9,8 @@ class RainbowSparkle : public Pattern {
     CRGB paintLed (float position, float remaining, CRGB previous, int primaryHue) {
 
       // println("position:" + position +", remaining:" + remaining);
-      if (random(100) < 10)
-      return CRGB(position * 255, 128* remaining, 128 * remaining);
+      if (random(100) < 10) 
+      return CRGB(position * 255, 128* (remaining + 0.2), 128 * (remaining + 0.2));
 
       return previous.nscale8(32);
 
